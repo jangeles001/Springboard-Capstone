4d64ca3756c81896d26d46b049ee47c8aba1568c
jangeles001 <207365281+jangeles001@users.noreply.github.com>
2026-02-21 00:07:13 -0800

    Changed env config in preparation for deployment

--- fitsync-fitness-tracker/server/src/config/redisClient.js
+++ fitsync-fitness-tracker/server/src/config/redisClient.js
@@ -1,12 +1,24 @@
 import { createClient } from "redis";
 import { getEnv } from "./envConfig.js";
 
+const REDIS_URL =
+  getEnv("NODE_ENV") === "production"
+    ? getEnv("UPSTASH_REDIS_REST_URL")
+    : getEnv("REDIS_TEST_URL");
+
+const REDIS_TOKEN =
+  getEnv("NODE_ENV") === "production"
+    ? getEnv("UPSTASH_REDIS_REST_TOKEN")
+    : null;
+
 const redisClient = createClient({
-  url: getEnv("REDIS_URL") || "redis://localhost:6379", // REDIS_URL the client with try to connect to
+  url: REDIS_URL,
+  token: REDIS_TOKEN,
 });
 
-redisClient.on("error", (err) => console.error("Redis Client Error", err)); // Catches redisClient errors
+redisClient.on("error", (err) => console.error("Redis Client Error", err));
 
-await redisClient.connect(); // Attempts to connect to redisClient
+await redisClient.connect();
+console.log("REDIS connection established");
 
 export default redisClient;
--- fitsync-fitness-tracker/server/src/schemas/envConfigZodSchema.js
+++ fitsync-fitness-tracker/server/src/schemas/envConfigZodSchema.js
@@ -2,7 +2,9 @@ import { z } from "zod";
 
 export const envConfigZodSchema = z.object({
   CLIENT_ORIGIN: z.string(),
-  NODE_ENV: z.enum(["production", "development", "test"]).default("development"),
+  NODE_ENV: z
+    .enum(["production", "development", "test"])
+    .default("development"),
   MONGO_URI: z
     .string()
     .startsWith("mongodb", "Mongodb url should start with mongodb:"),
@@ -13,25 +15,17 @@ export const envConfigZodSchema = z.object({
   JWT_SECRET: z
     .string()
     .min(8, "JWT_SECRET needs to be at minimum 8 characters"),
-  REDIS_URL: z
+  REDIS_TEST_URL: z
     .string()
     .startsWith("redis", "Redis url should start with redis:"),
-  RECAPTCHA_SECRET_KEY: z
-  .string()
-  .min(1, "RECAPTCHA_SECRET_KEY is required"),
-  GEMINI_API_KEY: z
-  .string()
-  .min(1, "GEMINI_API_KEY is required"),
-  MAILJET_API_KEY: z
-  .string()
-  .min(1, "MAILJET_API_KEY is required"),
-  MAILJET_SECRET_KEY: z
-  .string()
-  .min(1, "MAILJET_SECRET_KEY is required") ,
-  MAILJET_FROM_NAME: z
-  .string()
-  .min(1, "MAILJET_FROM_NAME is required"), 
+  UPSTASH_REDIS_REST_URL: z.string().min(1, "UPSTASH REST url is required"),
+  UPSTASH_REDIS_REST_TOKEN: z.string().min(1, "UPSTASH REST token is required"),
+  RECAPTCHA_SECRET_KEY: z.string().min(1, "RECAPTCHA_SECRET_KEY is required"),
+  GEMINI_API_KEY: z.string().min(1, "GEMINI_API_KEY is required"),
+  MAILJET_API_KEY: z.string().min(1, "MAILJET_API_KEY is required"),
+  MAILJET_SECRET_KEY: z.string().min(1, "MAILJET_SECRET_KEY is required"),
+  MAILJET_FROM_NAME: z.string().min(1, "MAILJET_FROM_NAME is required"),
   MAILJET_FROM_EMAIL: z
-  .string()
-  .email("MAILJET_FROM_EMAIL must be a valid email address"),
+    .string()
+    .email("MAILJET_FROM_EMAIL must be a valid email address"),
 });
