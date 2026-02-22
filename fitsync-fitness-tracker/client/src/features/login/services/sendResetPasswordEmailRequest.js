dc5cfb003a5644a14241f5249a1f16d848ec2cbe
jangeles001 <207365281+jangeles001@users.noreply.github.com>
2026-02-21 19:44:06 -0800

    Added guard for null response to log in fetching services and removed unused values from loing form custom hook

--- fitsync-fitness-tracker/client/src/features/login/services/sendResetPasswordEmailRequest.js
+++ fitsync-fitness-tracker/client/src/features/login/services/sendResetPasswordEmailRequest.js
@@ -1,7 +1,7 @@
-import { api } from '../../../services/api';
+import { api } from "../../../services/api";
 
 export function sendResetPasswordEmailRequest(email) {
-    const response = api.post(`api/v1/auth/reset-password/`, { email });
-    
-    return response.data;
-} 
+  const response = api.post(`api/v1/auth/reset-password/`, { email });
+
+  return response?.data;
+}
--- fitsync-fitness-tracker/client/src/features/login/services/sendResetPasswordRequest.js
+++ fitsync-fitness-tracker/client/src/features/login/services/sendResetPasswordRequest.js
@@ -1,6 +1,9 @@
 import { api } from "../../../services/api";
 
 export function sendResetPasswordRequest(token, password) {
-    const response = api.patch(`api/v1/auth/reset-password/${token}`, { password });
-    return response.data;
+  const response = api.patch(`api/v1/auth/reset-password/${token}`, {
+    password,
+  });
+
+  return response?.data;
 }
